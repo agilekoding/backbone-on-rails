59be506ccca8224474fa1f5b734450679c63dd5d
Sergio Morales <sergio.morales@agilekoding.com>
2013-01-09 16:37:34 -0600

    Changed the extensionof scaffolded templates to .hbs.haml

--- lib/generators/backbone/scaffold/scaffold_generator.rb
+++ lib/generators/backbone/scaffold/scaffold_generator.rb
@@ -22,7 +22,7 @@ module Backbone
       def parse_options
         js = options.javascript
         @ext  = js ? ".js" : ".js.coffee"
-        @tmpl = ".handlebars.haml"
+        @tmpl = ".hbs.haml"
         @templates_namespace = "HandlebarsTemplates"
       end
 
